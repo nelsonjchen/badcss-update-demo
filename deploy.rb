2e3af92e2c10bf7041b46c459c4e6aad8d1b3dd0
Nelson Chen <crazysim@gmail.com>
2012-08-06 01:12:01 -0700

    simplify test

--- deploy.rb
+++ deploy.rb
@@ -1,2 +1,6 @@
 import praw
-
+r = praw.Reddit("bad css uploader")
+r.login("PyApiTestUser2","1111")
+sr = r.get_subreddit("reddit_api_test")
+sr.set_stylesheet("invalid css example string")
+print(sr.get_stylesheet())
